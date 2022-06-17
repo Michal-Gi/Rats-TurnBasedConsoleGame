7552a6c61a03c46765094f64293028424ac550e5
Michal-Gi <michalg394@gmail.com>
2022-06-17 19:39:42 +0200

    restored project to previous form
    after deleting duplicate files the program stopped working

---
+++ 2122_PJC_pl_s24333/cmake-build-debug/CMakeFiles/2122_PJC_pl_s24333.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Michnik tech tips/Documents/2122_PJC_pl_s24333/Rats-TurnBasedConsoleGame/2122_PJC_pl_s24333/Rat.cpp" "CMakeFiles/2122_PJC_pl_s24333.dir/Rat.cpp.obj" "gcc" "CMakeFiles/2122_PJC_pl_s24333.dir/Rat.cpp.obj.d"
+  "C:/Users/Michnik tech tips/Documents/2122_PJC_pl_s24333/Rats-TurnBasedConsoleGame/2122_PJC_pl_s24333/Rats.cpp" "CMakeFiles/2122_PJC_pl_s24333.dir/Rats.cpp.obj" "gcc" "CMakeFiles/2122_PJC_pl_s24333.dir/Rats.cpp.obj.d"
+  "C:/Users/Michnik tech tips/Documents/2122_PJC_pl_s24333/Rats-TurnBasedConsoleGame/2122_PJC_pl_s24333/main.cpp" "CMakeFiles/2122_PJC_pl_s24333.dir/main.cpp.obj" "gcc" "CMakeFiles/2122_PJC_pl_s24333.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2122_PJC_pl_s24333/cmake-build-debug/CMakeFiles/2122_PJC_pl_s24333.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "2122_PJC_pl_s24333.exe"
+  "2122_PJC_pl_s24333.exe.manifest"
+  "2122_PJC_pl_s24333.pdb"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/Rat.cpp.obj"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/Rat.cpp.obj.d"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/Rats.cpp.obj"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/Rats.cpp.obj.d"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/main.cpp.obj"
+  "CMakeFiles/2122_PJC_pl_s24333.dir/main.cpp.obj.d"
+  "lib2122_PJC_pl_s24333.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2122_PJC_pl_s24333.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
